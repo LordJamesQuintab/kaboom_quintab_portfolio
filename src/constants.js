f7436745289c0b6358a6040a42686a0254a269ff
unknown <jamjaminquintab@gmail.com>
2024-08-29 02:52:44 +0800

    basic kaboom

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. Here i can do code and have a stressful night.
+    Here is my <a href="https://github.com/LordJamesQuintab" target="_blank">Github</a>!`,
+  "cs-degree": `This is my BSIT degree 
+<a href="jam.png" target="_blank">This is Me.</a>
+. I hung it on the wall because I'm proud of it. `,
+  "sofa-table": `That's my sofa. I like to relax here and and make guitar covers. 
+  I also make guitar covers. Go follow to <a href="https://www.tiktok.com/@jeymiks11?lang=en" target="_blank">my tiktok account</a>! (If you like the content)!`,
+  tv: `That's my TV. I've been watching some Movies and Anime like :
+  Deadpool and Wolverine , One Piece and others!`,
+  bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my porfolio here you can see my resume. <a href="https://quintabmp1.netlify.app/" target="_blank">Check it out?</a>
+  Contact me at jamixgino@gmail.com if you have any interesting job opportunities!`,
+  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas.`,
+  library: `There are a lot of programming books on my shelves. T
+  I probably only read one of them when i'm get bored. `,
+  exit: `If you want to exit Lord James's portfolio, just close the tab.`,
+};
